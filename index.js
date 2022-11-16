39cb814a6aceaea7644f848dfff874b59fbe4bbe
Tommy <ytommy0111@aol.com>
2022-11-16 10:13:56 -0500

    grglgll

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const button = document.getElementById("btn");
+const DOMSelectors = {
+  button: document.querySelector("btn"),
+  box: document.querySelectorAll("text-box"),
+  text: document.querySelectorAll("text"),
+  input: document.querySelectorAll("form-inputs"),
+};
+
+console.log(DOMSelectors);
+
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  console.log(input);
+});
