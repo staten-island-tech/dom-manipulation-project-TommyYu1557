3fb9563d631d566aa14ef4bdf400f86e9f68cf1f
Tommy <ytommy0111@aol.com>
2022-11-18 10:16:07 -0500

    why is this not working >:(

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const DOMSelectors = {
-  button: document.querySelector("btn"),
+  button: document.getElementByID("btn"),
   box: document.querySelectorAll("text-box"),
   text: document.querySelectorAll("text"),
-  input: document.querySelectorAll("form-inputs"),
+  input1: document.querySelector("input1"),
+  input2: document.querySelector("input2"),
+  input3: document.querySelector("input3"),
+  form: document.querySelectorAll("form"),
 };
 
 console.log(DOMSelectors);
 
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.form.addEventListener("submit", function () {
   let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", "<p> $(input)$ </p>");
   console.log(input);
 });
