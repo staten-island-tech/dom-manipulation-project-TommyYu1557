11bb652e374bc9484b76fcdd1beebca19f060582
Tommy <ytommy0111@aol.com>
2022-11-21 10:15:59 -0500

    yes

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const DOMSelectors = {
-  button: document.getElementByID("btn"),
+  button: document.getElementsByClassName("btn"),
   box: document.querySelectorAll("text-box"),
   text: document.querySelectorAll("text"),
-  input1: document.querySelector("input1"),
-  input2: document.querySelector("input2"),
-  input3: document.querySelector("input3"),
-  form: document.querySelectorAll("form"),
+  input1: document.querySelector("textbox1"),
+  input2: document.querySelector("textbox2"),
+  input3: document.querySelector("textbox3"),
+  form: document.getElementById("form"),
+  display: document.querySelector("display"),
 };
 
 console.log(DOMSelectors);
 
 DOMSelectors.form.addEventListener("submit", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", "<p> $(input)$ </p>");
-  console.log(input);
+  let input1 = DOMSelectors.input1.value,
+    input2 = DOMSelectors.input2.value,
+    input3 = DOMSelectors.input3.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", "<p> $(.input1)$ </p>");
+  DOMSelectors.box.insertAdjacentHTML("beforeend", "<p> $(.input1)$ </p>");
+  DOMSelectors.box.insertAdjacentHTML("beforeend", "<p> $(.input1)$ </p>");
+  console.log(input1, input2, input3);
+});
+
+DOMSelectors.form.addEventListener("submit", function (bruh) {
+  bruh.preventDefault();
 });
